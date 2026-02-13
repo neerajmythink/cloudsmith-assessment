6f789312899d8d82b3de55f770462e3390af211c
Neeraj.Srivastava <nsrivastava@cloudsmith.io>
2026-02-13 19:49:26 +0530

    added team and service account with privileges

--- cloudsmith-api-assessment/repo_add_privileges.sh
+++ cloudsmith-api-assessment/repo_add_privileges.sh
@@ -10,70 +10,85 @@ export NAMESPACE="cloudsmith-org-neeraj" # Replace with your actual namespace
 export API_KEY=$CLOUDSMITH_API_KEY # Ensure you have set the CLOUDSMITH_API_KEY environment variable with your API key
 export REPO_NAME="assessment-repo" # You can change the repository name as needed
 export TEAM_NAME="new-team" # Name for the team to be created
-export SERVICE_NAME="new_service" # Name for the service account to be created
+export SERVICE_NAME="new-service" # Name for the service account to be created
 
-echo "#### Adding team to the namespace '$NAMESPACE' ####"
-curl -sS\
-  --request POST \
-  --url "https://api.cloudsmith.io/orgs/${NAMESPACE}/teams/" \
-  --header 'accept: application/json' \
-  --header 'content-type: application/json' \
-  --header "X-Api-Key: ${API_KEY}" \
-  --data '
-{
-  "visibility": "Visible",
-  "description": "team created using API",
-  "name":"'${TEAM_NAME}'"
-} ' | jq
-
-echo "#### Adding Write access to the repository '$REPO_NAME' for team '$TEAM_NAME' in namespace '$NAMESPACE' ####"
-curl -sS\
-  --request PATCH \
-  --url https://api.cloudsmith.io/repos/${NAMESPACE}/${REPO_NAME}/privileges \
-  --header "X-Api-Key: ${API_KEY}" \
-  --header 'accept: application/json' \
-  --header 'content-type: application/json' \
-  --data '
-{
-  "privileges": [
-    {
-      "privilege": "Write",
-      "team": "'${TEAM_NAME}'"
-    }
-  ]
+# function to add team to the namespace
+add_team() {
+  curl -sS\
+    --request POST \
+    --url "https://api.cloudsmith.io/orgs/${NAMESPACE}/teams/" \
+    --header 'accept: application/json' \
+    --header 'content-type: application/json' \
+    --header "X-Api-Key: ${API_KEY}" \
+    --data '
+            {
+              "visibility": "Visible",
+              "description": "team created using API",
+              "name":"'${TEAM_NAME}'"
+            } ' | jq
 }
-'
 
-# echo "#### Adding service account with Admin access to the repository $REPO_NAME in namespace $NAMESPACE ####"
+# function to add privileges to team for the repository with write access
+add_team_privileges() {
+  curl -sS\
+    --request PATCH \
+    --url https://api.cloudsmith.io/repos/${NAMESPACE}/${REPO_NAME}/privileges \
+    --header "X-Api-Key: ${API_KEY}" \
+    --header 'accept: application/json' \
+    --header 'content-type: application/json' \
+    --data '
+          {
+            "privileges": [
+              {
+                "privilege": "Write",
+                "team": "'${TEAM_NAME}'"
+              }
+            ]
+          }
+          ' | jq
+}
 
-# curl -sS\
-#   --request POST \
-#   --url https://api.cloudsmith.io/orgs/${NAMESPACE}/services/ \
-#   --header 'accept: application/json' \
-#   --header 'content-type: application/json' \
-#   --header "X-Api-Key: ${API_KEY}" \
-#   --data '
-# {
-#   "role": "Member",
-#   "description": "service account created using API",
-#   "name": "'${SERVICE_NAME}'"
-# }
-# ' | jq
+# function to add service account to the namespace
+add_service_account() {
+  curl -sS\
+    --request POST \
+    --url https://api.cloudsmith.io/orgs/${NAMESPACE}/services/ \
+    --header 'accept: application/json' \
+    --header 'content-type: application/json' \
+    --header "X-Api-Key: ${API_KEY}" \
+    --data '
+            {
+              "description": "service account created using API",
+              "name": "'${SERVICE_NAME}'"
+            }
+            ' | jq -r '.slug'
+}
 
-# curl -sS\
-#   --request PATCH \
-#   --url https://api.cloudsmith.io/repos/${NAMESPACE}/${REPO_NAME}/privileges \
-#   --header 'accept: application/json' \
-#   --header 'content-type: application/json' \
-#   --data '
-# {
-#   "privileges": [
-#     {
-#       "privilege": "Admin",
-#       "service": "'${SERVICE_NAME}'"
-#     }
-#   ]
-# }
-# ' | jq
+# function to add privileges to the repository for service account
+add_service_account_privileges() {
+  curl -sS\
+    --request PATCH \
+    --url https://api.cloudsmith.io/repos/${NAMESPACE}/${REPO_NAME}/privileges \
+    --header 'accept: application/json' \
+    --header 'content-type: application/json' \
+    --header "X-Api-Key: ${API_KEY}" \
+    --data '
+            {
+              "privileges": [
+                {
+                  "privilege": "Admin",
+                  "service": "'${SERVICE_ACCOUNT_SLUG}'"
+                }
+              ]
+            }
+            '
+}
 
+echo "#### Adding a team to the namespace '$NAMESPACE' and giving Write access to '$TEAM_NAME' team for the repository '$REPO_NAME' ####"
+add_team
+add_team_privileges
 
+echo "#### Adding a service account to the namespace '$NAMESPACE' and giving Admin access to '$SERVICE_NAME' account for the repository '$REPO_NAME' ####"
+export SERVICE_ACCOUNT_SLUG=$(add_service_account)
+echo "Service account slug: $SERVICE_ACCOUNT_SLUG"
+add_service_account_privileges
