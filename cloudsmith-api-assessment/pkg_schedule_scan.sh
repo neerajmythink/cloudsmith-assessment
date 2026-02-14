b7ffeabf5d68a1c01bc85630550d5ad2f4db47a9
Neeraj.Srivastava <nsrivastava@cloudsmith.io>
2026-02-14 21:50:33 +0530

    fixed doc

--- cloudsmith-api-assessment/pkg_schedule_scan.sh
+++ cloudsmith-api-assessment/pkg_schedule_scan.sh
@@ -4,7 +4,7 @@
 export NAMESPACE="cloudsmith-org-neeraj" # Replace with your actual namespace
 export REPO_NAME="assessment-repo" # You can change the repository name as needed
 export API_KEY=$CLOUDSMITH_API_KEY # Ensure you have set the CLOUDSMITH_API_KEY environment variable with your API key
-export PKG_IDENTIFIER="cloudsmith_python_native_example-100-py2py3-n-5s3z" # Replace with the actual package identifier you want to copy
+export PKG_IDENTIFIER="cloudsmith_python_native_example-100-py2py3-n-5s3z" # Replace with the actual package identifier you want to schedule scan
 
 # Function to schedule a scan for an existing package in the repository
 schedule_scan() {
