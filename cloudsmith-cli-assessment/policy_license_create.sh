983430c345a5bb9b4e0fb56e097915f76ff7891e
Neeraj.Srivastava <nsrivastava@cloudsmith.io>
2026-02-16 00:20:19 +0530

    renamed some files

--- cloudsmith-cli-assessment/policy_license_create.sh
+++ cloudsmith-cli-assessment/policy_license_create.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# cloudsmith cli command to Create a new Licence Policy. Document the steps and configurations you chose.
+# cloudsmith cli command to Create a new Licence Policy.
 
 NAMESPACE="cloudsmith-org-neeraj"  # Your Cloudsmith namespace 
-REPO_NAME="example_repo_through_cli"  # The name of the repository where the package currently exists
-
-cloudsmith policy license create ${NAMESPACE} policy_license_create.json
+cloudsmith policy license create ${NAMESPACE} policy_license_config.json
