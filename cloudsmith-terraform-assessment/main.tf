5a27a157b54b6379777d5b9c878bfc6661c98952
Neeraj.Srivastava <nsrivastava@cloudsmith.io>
2026-02-17 11:56:15 +0530

    added upstreams to qa and staging

--- cloudsmith-terraform-assessment/main.tf
+++ cloudsmith-terraform-assessment/main.tf
@@ -29,3 +29,29 @@ resource "cloudsmith_repository" "production" {
   namespace   = var.organization
   description = "Production repository created using Terraform"
 }
+
+# Add upstreams to QA repository for npm and maven
+resource "cloudsmith_repository_upstream" "qa_npm" {
+  repository    = cloudsmith_repository.qa.slug
+  namespace     = var.organization
+  name          = "npm-upstream"
+  upstream_type = "npm"
+  upstream_url  = "https://registry.npmjs.org"
+}
+
+resource "cloudsmith_repository_upstream" "qa_maven" {
+  repository    = cloudsmith_repository.qa.slug
+  namespace     = var.organization
+  name          = "maven-upstream"
+  upstream_type = "maven"
+  upstream_url  = "https://repo.maven.apache.org/maven2"
+}
+
+# Add upstream to Staging repository for python
+resource "cloudsmith_repository_upstream" "staging_python" {
+  repository    = cloudsmith_repository.staging.slug
+  namespace     = var.organization
+  name          = "python-upstream"
+  upstream_type = "python"
+  upstream_url  = "https://pypi.org/simple"
+}
